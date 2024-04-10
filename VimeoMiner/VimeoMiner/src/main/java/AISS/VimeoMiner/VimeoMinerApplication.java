54ebe575a045a56e5649457ad03bc01739d3c7c5
paugonpae67 <114425589+paugonpae67@users.noreply.github.com>
2024-04-10 11:19:10 +0200

    Add files via upload

---
+++ VimeoMiner/VimeoMiner/src/main/java/AISS/VimeoMiner/VimeoMinerApplication.java
@@ -0,0 +1,13 @@
+package AISS.VimeoMiner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VimeoMinerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VimeoMinerApplication.class, args);
+	}
+
+}
